287c89c803914946137edbb1dbaab9a9bde823bc
Matthew Bekkema <mbekkema97@gmail.com>
2015-11-20 11:05:39 +1100

    Separate Windows code and begin work on X11

--- System/Clipboard.hs
+++ System/Clipboard.hs
@@ -1,97 +1,30 @@
-
 {-# LANGUAGE CPP #-}
 
--- | System clipboard interface with unicode support.
---
--- For more information, see "Graphics.Win32.GDI.Clip"
--- or documentation for /GetClipboardData/ on MSDN.
 module System.Clipboard
-    ( 
-#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+    (
       -- * Clipboard interface
       setClipboardString
     , getClipboardString
     , modifyClipboardString
-      -- * Clipboard format
-    , cF_UNICODETEXT
-#else
-      -- * Undefined
-
-      -- | This library currently only works in Windows.
-      --   If you want to contribute, please, don't hesitate
-      --   in visit the Git repository at <https://github.com/Daniel-Diaz/Clipboard>.
-#endif
     ) where
 
 #if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+import qualified System.Clipboard.Windows as OS
+#else
+import qualified System.Clipboard.X11 as OS
+#endif
 
-import System.Win32.Mem 
-    (globalAlloc, globalLock, globalUnlock, copyMemory, gHND)
-import Graphics.Win32.GDI.Clip 
-    ( openClipboard, closeClipboard, emptyClipboard, 
-      getClipboardData, setClipboardData
-    , cF_TEXT, ClipboardFormat 
-    , isClipboardFormatAvailable)
-import Foreign.C
-   (withCAString, peekCAString, withCWString, peekCWString)
-import Foreign.Ptr
-    (castPtr, nullPtr)
-import Data.List
-    (genericLength)
-import Control.Exception 
-    (bracket_, bracket)
-import Data.Maybe
-    (isJust)
-
--- | Clipboard format of Unicode text.
-cF_UNICODETEXT :: ClipboardFormat
-cF_UNICODETEXT = 13
-
--- | Puts a string on the clipboard, using the 'cF_UNICODETEXT' clipboard format.
+-- | Puts a string on the clipboard.
 setClipboardString :: String -> IO ()
-setClipboardString str =
-   withCWString str $ \cstring -> do
-   mem <- globalAlloc gHND strLen
-   bracket (globalLock mem) globalUnlock $ \mem' -> do
-       copyMemory mem' (castPtr cstring) strLen
-       bracket_ (openClipboard nullPtr) closeClipboard $ do
-           emptyClipboard
-           setClipboardData cF_UNICODETEXT mem'
-           return ()
- where
-   strLen = 2 * (genericLength str + 1)
+setClipboardString = OS.setClipboardString
 
 -- | Gets the contents of the clipboard as a 'String'.
--- Returns 'Nothing' if the clipboard content is not
--- of type 'cF_TEXT' or 'cF_UNICODETEXT', i.e. it
--- doesn't contain /textual/ data.
+-- Returns 'Nothing' if the clipboard doesn't contain /textual/ data.
 getClipboardString :: IO (Maybe String)
-getClipboardString =
-   bracket_ (openClipboard nullPtr) closeClipboard $ do
-     isUnicodeAvailable <- isClipboardFormatAvailable cF_UNICODETEXT
-     if isUnicodeAvailable
-       then do handle <- getClipboardData cF_UNICODETEXT
-               mem <- globalLock handle
-               str <- peekCWString (castPtr mem)
-               globalUnlock mem
-               return $ Just str
-       else do isAnsiAvailable <- isClipboardFormatAvailable cF_TEXT
-               if isAnsiAvailable
-                 then do handle <- getClipboardData cF_TEXT
-                         mem <- globalLock handle
-                         str <- peekCAString (castPtr mem)
-                         globalUnlock mem
-                         return $ Just str
-                 else return Nothing
+getClipboardString = OS.getClipboardString
 
 -- | Modifies the clipboard content.
 -- If the clipboard has /textual/ data, this function modifies its content,
 -- and return 'True'. Otherwise, it does nothing and return 'False'.
 modifyClipboardString :: (String -> String) -> IO Bool
-modifyClipboardString f = do
- s <- getClipboardString
- case s of
-   Nothing -> return False
-   Just sc -> setClipboardString (f sc) >> return True
-
-#endif
+modifyClipboardString = OS.modifyClipboardString
---
+++ System/Clipboard/X11.hs
@@ -0,0 +1,45 @@
+module System.Clipboard.X11
+( getClipboardString
+, setClipboardString
+, modifyClipboardString
+) where
+
+import Control.Monad
+import Control.Concurrent
+import Graphics.X11.Xlib.Extras
+import Graphics.X11.Xlib
+import Data.Functor
+import Codec.Binary.UTF8.String
+
+setClipboardString = undefined
+modifyClipboardString = undefined
+
+getClipboardString :: IO (Maybe String)
+getClipboardString = withInitialSetup $ \display window -> do
+    clipboard <- internAtom display "CLIPBOARD" True
+    inp <- internAtom display "clipboard_get" False
+    target <- internAtom display "UTF8_STRING" True
+    xConvertSelection display clipboard target inp window currentTime
+    clipboardInputWait display window inp
+
+clipboardInputWait :: Display -> Window -> Atom -> IO (Maybe String)
+clipboardInputWait display window inp = do
+    ev <- getNextEvent display
+    if ev_event_type ev == selectionNotify
+        then (charsToString <$>) <$> getWindowProperty8 display inp window
+        else clipboardInputWait display window inp
+
+charsToString = decode . map fromIntegral
+
+withInitialSetup :: (Display -> Window -> IO a) -> IO a
+withInitialSetup f = do
+    display <- openDisplay ""
+    window <- createSimpleWindow display (defaultRootWindow display) 0 0 1 1 0 0 0
+    ret <- f display window
+    destroyWindow display window
+    closeDisplay display
+    return ret
+
+getNextEvent display = allocaXEvent $ \ev -> do
+    nextEvent display ev
+    getEvent ev
